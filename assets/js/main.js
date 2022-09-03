102a00c99b71916252fc1be8cc9e32f81c13a953
“BiankaFranke” <bianka_franke@hotmail.de>
2022-09-03 15:06:41 +0200

    Added js and some css

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,9 +20,10 @@ let temp = document.querySelector("#cityTemp");
 let tempMin = document.querySelector("#cityTempMin");
 let tempMax = document.querySelector("#cityTempMax");
 let windSpeed = document.querySelector("#cityWindSpeed");
-let windDeg = document.querySelector("#cityWindDef");
+let windDeg = document.querySelector("#cityWindDeg");
 let date = document.querySelector("#date");
-let cityName = document.querySelector('#name');
+let cityName = document.querySelectorAll('.name');
+let label = document.querySelectorAll('.label');
 let icon = document.querySelector('#icon');
 let rain = document.querySelector('#rain');
 let coordsLonInput = document.querySelector('#lon');
@@ -31,8 +32,7 @@ let coordsLatInput = document.querySelector('#lat');
 search.addEventListener('click', (event) => {
     event.preventDefault();
     let locationInput = locationTest.value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput}&units=metric&appid=${api}`;
-
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput}&units=metric&appid=${api}`
 
     fetch(url)
         .then((response) => response.json())
@@ -45,8 +45,7 @@ search.addEventListener('click', (event) => {
             let windSpeedInput = data['wind']['speed'];
             let windDegInput = data['wind']['deg'];
             let clouds = data['clouds']['all'];
-            // let rainInput = data['rain']['1h'];
-            let iconInput = data['weather']['icon'];
+            let iconInput = data['weather']['0']['icon'];
             let pressureInput = data['main']['pressure'];
             let humidityInput = data['main']['humidity'];
             let sunriseInput = data['sys']['sunrise'];
@@ -54,26 +53,33 @@ search.addEventListener('click', (event) => {
             let countryInput= data['sys']['country'];
             let coordsLon= data['coord']['lon'];
             let coordsLat= data['coord']['lat'];
+           
+            for (let i = 0; i < cityName.length; i++) {
+                cityName[i].innerHTML  = nameValue;
+            }
 
+            for (let j = 0; j < label.length; j++) {
+                label[j].style.display = "block";
+            }
 
-            cityName.innerHTML = nameValue;
             country.innerHTML = countryInput;
             cloudDescription.innerHTML = descriptionValue;
-            icon.innerHTML = iconInput;
-            // rain.innerHTML = rainInput;
+            cloudiness.innerHTML = clouds;
+            icon.innerHTML= `<img src='https://openweathermap.org/img/wn/${iconInput}.png' alt="test">`;
+            
             temp.innerHTML = `${tempValue.toFixed(1)} °C`;
             tempMin.innerHTML = `${tempValueMin.toFixed(1)} °C`;
             tempMax.innerHTML = `${tempValueMax.toFixed(1)} °C`;
             date.innerHTML = `${today.toLocaleTimeString()} ${today.toDateString()}`;
             pressure.innerHTML = `${pressureInput} hpa`;
             humidity.innerHTML = `${humidityInput} %`;
-            pressure.innerHTML = `${windSpeedInput} m/s`;
-            humidity.innerHTML = `${windDegInput} deg`;
-
+            windSpeed.innerHTML = `${windSpeedInput} m/s`;
+            windDeg.innerHTML = `${windDegInput} deg`;
             sunrise.innerHTML = new Date(sunriseInput).toLocaleTimeString();
             sunset.innerHTML = new Date(sunsetInput).toLocaleTimeString();
+            coordsLonInput.innerHTML = coordsLon.toFixed(2);
+            coordsLatInput.innerHTML = coordsLat.toFixed(2);
 
-            coordsLonInput.innerHTML = coordsLon;
-            coordsLatInput.innerHTML = coordsLat;
+            console.log(today.toUTCString())
     });
 });
