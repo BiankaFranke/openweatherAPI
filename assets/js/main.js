692be59e1fb7c6a891f107a168265b2eaff8cfd7
“BiankaFranke” <bianka_franke@hotmail.de>
2022-09-07 19:40:43 +0200

    upload without api key

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,11 @@
 console.log('it works');
 
 // Declarations global
-const api = 'cdf4e4cb66d336dbea48d94a6940ce7c';
+const api = 'add api key here';
 let locationTest = document.querySelector('#location');
 let search = document.querySelector('#search');
-// let app = document.querySelector('#weatherApp');
 let today = new Date();
-
 let country = document.querySelector("#cityCountry");
-let time = document.querySelector("#cityTime");
 let cloudiness = document.querySelector("#cloudiness");
 let cloudDescription = document.querySelector("#cloud");
 let humidity = document.querySelector("#humidity");
@@ -25,14 +22,22 @@ let date = document.querySelector("#date");
 let cityName = document.querySelectorAll('.name');
 let label = document.querySelectorAll('.label');
 let icon = document.querySelector('#icon');
-let rain = document.querySelector('#rain');
+// let rain = document.querySelector('#rain');
 let coordsLonInput = document.querySelector('#lon');
 let coordsLatInput = document.querySelector('#lat');
 
 search.addEventListener('click', (event) => {
     event.preventDefault();
+
     let locationInput = locationTest.value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput}&units=metric&appid=${api}`
+
+    if (locationInput == '') {
+        locationInput = 'Berlin';
+    } else {
+        locationInput = locationTest.value;
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput}&units=metric&appid=${api}`;
 
     fetch(url)
         .then((response) => response.json())
@@ -50,9 +55,9 @@ search.addEventListener('click', (event) => {
             let humidityInput = data['main']['humidity'];
             let sunriseInput = data['sys']['sunrise'];
             let sunsetInput = data['sys']['sunset'];
-            let countryInput= data['sys']['country'];
-            let coordsLon= data['coord']['lon'];
-            let coordsLat= data['coord']['lat'];
+            let countryInput = data['sys']['country'];
+            let coordsLon = data['coord']['lon'];
+            let coordsLat = data['coord']['lat'];
            
             for (let i = 0; i < cityName.length; i++) {
                 cityName[i].innerHTML  = nameValue;
@@ -64,22 +69,20 @@ search.addEventListener('click', (event) => {
 
             country.innerHTML = countryInput;
             cloudDescription.innerHTML = descriptionValue;
-            cloudiness.innerHTML = clouds;
-            icon.innerHTML= `<img src='https://openweathermap.org/img/wn/${iconInput}.png' alt="test">`;
+            cloudiness.innerHTML = `${clouds} %`;
+            icon.innerHTML= `<img src='https://openweathermap.org/img/wn/${iconInput}@2x.png' alt="test">`;
             
-            temp.innerHTML = `${tempValue.toFixed(1)} °C`;
-            tempMin.innerHTML = `${tempValueMin.toFixed(1)} °C`;
-            tempMax.innerHTML = `${tempValueMax.toFixed(1)} °C`;
-            date.innerHTML = `${today.toLocaleTimeString()} ${today.toDateString()}`;
-            pressure.innerHTML = `${pressureInput} hpa`;
-            humidity.innerHTML = `${humidityInput} %`;
+            temp.innerHTML = ` ${tempValue.toFixed(1)} °C`;
+            tempMin.innerHTML = ` ${tempValueMin.toFixed(1)} °C`;
+            tempMax.innerHTML = ` ${tempValueMax.toFixed(1)} °C`;
+            date.innerHTML = ` ${today.toLocaleTimeString()} ${today.toDateString()}`;
+            pressure.innerHTML = ` ${pressureInput} hpa`;
+            humidity.innerHTML = ` ${humidityInput} %`;
             windSpeed.innerHTML = `${windSpeedInput} m/s`;
-            windDeg.innerHTML = `${windDegInput} deg`;
-            sunrise.innerHTML = new Date(sunriseInput).toLocaleTimeString();
-            sunset.innerHTML = new Date(sunsetInput).toLocaleTimeString();
+            windDeg.innerHTML = ` ${windDegInput} deg`;
+            sunrise.innerHTML = new Date(sunriseInput * 1000).toLocaleTimeString();
+            sunset.innerHTML = new Date(sunsetInput * 1000).toLocaleTimeString();
             coordsLonInput.innerHTML = coordsLon.toFixed(2);
             coordsLatInput.innerHTML = coordsLat.toFixed(2);
-
-            console.log(today.toUTCString())
     });
 });
